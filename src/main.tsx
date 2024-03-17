3d61edea65ae3402671c91d06deeca90289d78e5
jbjalandoon <jerome.jalandoon@gmail.com>
2024-03-17 13:29:32 +0800

    add auth context to main router

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import {
+  AnyRoute,
+  Router,
+  RouterProvider,
+  createRouter,
+} from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+import { AuthenticationProvider, useAuth } from "./context/AuthContext";
 
 const router = createRouter({
   routeTree,
   context: {
-    user: {
-      id: null,
-      username: null,
-      email: null,
-      isAuthenticated: false,
-      role: null,
-    },
+    auth: undefined!,
   },
-});
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+}) as Router<AnyRoute, Record<string, any>, Record<string, any>>;
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+function InnerApp() {
+  const auth = useAuth();
+
+  return <RouterProvider router={router} context={{ auth }} />;
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthenticationProvider>
+      <InnerApp />
+    </AuthenticationProvider>
   </React.StrictMode>
 );
